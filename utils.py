c4447ec1c6b8ad7744f28f59e5205c123e1c16d0
JoseRH25 <jlgamer25.ah@gmail.com>
2022-11-14 19:39:31 -0600

    update utilis

--- utils.py
+++ utils.py
@@ -14,14 +14,18 @@ class Utils:
     def setConfig(self,config ):
         self.__config = config
     
+    
     def calculate_fitness(self, cromosoma):
         """ Metodo encargado de realizar el calculo del fitness
         """
+        numero =["1","2","3","4","5","6","7","8","9","0"]
         puntuacion = 0
         config = self.getConfig()
         stringPass= config["passcode"]["correct_passcode"]  
         cromosomaModelo = list(stringPass)
         for index in range(len(cromosomaModelo)):
+            if cromosomaModelo[index] in numero:
+                cromosomaModelo[index] = int(cromosomaModelo[index])
             if cromosomaModelo[index] == cromosoma[index]:
                 puntuacion += 1
         return puntuacion/len(cromosomaModelo)
