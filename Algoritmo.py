4dc06bf40534e6aa8094098eb0f3caf5d5a813e3
JoseRH25 <jlgamer25.ah@gmail.com>
2022-11-14 23:03:47 -0600

    metodo de seleccion

--- Algoritmo.py
+++ Algoritmo.py
@@ -2,27 +2,22 @@ class Algoritmo:
     """ Clase encargada de contener el algoritmo genetico 
     """
     
-    def __init__(self, config, poblacion):
-        self.config = config
+    def __init__(self, poblacion):
         self.poblacion = poblacion
 
+    
     def metodoRuleta(self, fitness):
+        print("fitness")
+        print(fitness)
         return []
     
     def metodoElite(self, elite):
+        print("fitness Elite")
         return []
     
     def metodoRanking(self, fitness):
+        print("fitness Ranking")
         return []
 
-    def metodoSeleccion(self,fitness ):
-        metodo = self.config["ag"]["selection_method"]
-        if metodo == "ruleta" :
-            self.metodoRuleta(fitness)  
-        elif metodo == "elite": 
-            self.metodoElite(fitness)
-        elif metodo == "ranking":
-            self.metodoRanking(fitness)
-        else:
-             raise Exception ("El método no se encuentra")
+   
   
